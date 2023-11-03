a744525be68716d8e4b4c83e5f9d310d51510818
Arun103k <arun.srinivasan.97@gmail.com>
2023-11-03 13:05:15 +0530

    1st commit

---
+++ Datatypes.py
@@ -0,0 +1,30 @@
+# integer variable.
+a=100
+print("The type of variable having value", a, " is ", type(a))
+
+# boolean variable.
+b=True
+print("The type of variable having value", b, " is ", type(b))
+
+# float variable.
+c=20.345
+print("The type of variable having value", c, " is ", type(c))
+
+# complex variable.
+d=10+3j
+print("The type of variable having value", d, " is ", type(d))
+
+g = 87.0
+print("the type of a variable having value of", g, "is",  type(g))
+
+e = {12,24,24.3,30}
+print("the type of variable having value of", e,"is", type(e))
+
+f = (13, 38, 40, 37)
+print("the type of variable is having value of", f, "is", type(f))
+
+h = ([1,2,3,4,5])
+print("the type of variable of having the value of", h, "is", type(h))
+
+i = ({1:"id",2:"number"})
+print("the type of variable of having the value of", i, "is", type(i))
