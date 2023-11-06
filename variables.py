a220312ac806135d95ec1a36103b7581e0f4e91c
Arun103k <arun.srinivasan.97@gmail.com>
2023-11-06 18:53:31 +0530

    3rd commit

---
+++ variables.py
@@ -0,0 +1,38 @@
+miles = 100
+counter = 12
+name = "wanderer"
+
+                                   #12
+print(name)                                       #wanderer
+print(miles)                                      #100
+
+# to delete the defined variables
+del counter
+del miles
+
+a = b = c = 360
+print(a)
+print(b)
+print(c)
+
+# area of the rectangle
+length = 12
+breadth = 20
+area = length * breadth
+print(area)
+
+# details of the employee
+name = "arun"
+role = "CFD analyst | cloud engineer"
+age = 25
+location = "bangalore"
+salary = 40000
+native = "mallasamudram"
+
+print(name)
+print(role)
+print(age)
+print(location)
+print(salary)
+print(native)
+
